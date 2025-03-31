72ef39fff61aa177bbb078c469528beaba3b1bb9
doom <1075101233doom@gmail.com>
2025-04-01 00:02:53 +0900

    [add]forever event

---
+++ src/net/forever_event.h
@@ -0,0 +1,19 @@
+#ifndef EMBER_NET_FOREVER_EVENT_H
+#define EMBER_NET_FOREVER_EVENT_H
+
+#include "event.h"
+
+namespace ember::net {
+
+class ForeverEvent : public Event {
+public:
+    explicit ForeverEvent(int id) : Event(id) {}
+
+    ~ForeverEvent() = default;
+
+    constexpr bool is_valid() const noexcept override { return true; }
+};
+
+} // namespace ember::net
+
+#endif // !EMBER_NET_FOREVER_EVENT_H
